76d864c71513e96e9c322ccf48929df979fa09ff
Raghav Vashisht <raghav2011rv@gmail.com>
2019-11-27 00:43:26 +0530

    added comments

--- src/emulator/Emulation.java
+++ src/emulator/Emulation.java
@@ -7,6 +7,31 @@ import java.io.IOException;
 
 public class Emulation {
 	
+	/*  
+	* 	Original Intel 8080 components:
+	*   PC is 16 bits (Unsigned)
+	*   SP is 16 bits (Unsigned)
+	*   All registers (A-E,H,L) and flags are 8 bit (Unsigned)
+	*   Memory is an array of Unsigned 8 bit with size being 0xffff.
+	*/  
+	
+	/*
+	 *  As Java does not support 8 bit or 16 bit "unsigned" integer types,
+	 *  We are using short instead of uint8 and int instead of uint16.
+	 *  
+	 *  Following Data will be used to take care of overflow. 
+	 *  { Overflow: 0xff + 1 == 0x00 if data type is uint8 but is 0x100 if the data type is more than 8 bits }
+	 *  { We take care of Overflow by doing a bitwise and (&). for example, 0x100 & 0xff = 0x00 }
+	 *  
+	 *  short is 16 bits (Signed)
+	 *  int is 32 bits (Signed)
+	 *  
+	 *  0xff(Hex) -> 255(Deciman) -> 11111111 (Binary) => 8 bits (1 Byte)
+	 *  0xffff(Hex) -> 65535 (Decimal) -> 1111111111111111 (Binary) => 16 bits (2 Bytes)
+	 *  
+	 *  Google if you don't know about signed and unsigned data types.
+	 */
+	
 	
 	void loadGame(CPU cpu, String name) {
 		File file = new File(System.getProperty("user.dir") + "/src/Games/" + name);
@@ -29,11 +54,26 @@ public class Emulation {
 		System.out.println("0x"+String.format("%02x", opcode));
 		switch(opcode) {
 			case 0x00: break; //NOP
-			case 0x01:
-				cpu.c = cpu.memory[cpu.pc+1];
-				cpu.b = cpu.memory[cpu.pc+2];
-				cpu.pc += 2;
+			
+			case 0x01: //LXI B,D16
+				cpu.c = cpu.memory[((cpu.pc+1) & 0xffff)];
+				cpu.b = cpu.memory[((cpu.pc+2) & 0xffff)];
+				cpu.pc = ((cpu.pc + 2) & 0xffff);
 				break;
+				
+			case 0x02: //STAX B
+				int x = (cpu.b & 0xff) << 8;
+				int y = cpu.c & 0xff;
+				int addr = x | y;
+				cpu.memory[addr] = (short) (cpu.a & 0xff);
+				break;
+				
+			case 0x03: //INX B
+				cpu.b = (short) ((cpu.b + 1) & 0xff);
+				cpu.c = (short) ((cpu.c + 1) & 0xff);
+				break;
+				
+			case 0x18: break; //NOP
 			default: System.out.println("UNIMPLEMENTED OPCODE: "+"0x"+String.format("%02x", opcode)); break;
 		}
 		cpu.pc += 1;
